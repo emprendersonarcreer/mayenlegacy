b30fc4e737dc18cc6cf7d86dfed17c6c11be616e
Nasheli Mayen <emprender.sonar.creer@gmail.com>
2026-05-26 13:31:05 -0700

    feat: add sitemap for Google Search Console

---
+++ app/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://mayenlegacy.com",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: "https://mayenlegacy.com/conferencista",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
+      url: "https://mayenlegacy.com/essentia",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://mayenlegacy.com/alkalination",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+  ];
+}
